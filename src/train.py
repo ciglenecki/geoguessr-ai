3057498329eec6b4e8b1c624327ddd860e5ea163
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2022-04-12 04:01:33 +0200

    add imagenet augment

--- src/train.py
+++ src/train.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
         [
             transforms.Resize(image_size),
             transforms.RandomHorizontalFlip(),
-            # transforms.AutoAugment(policy=AutoAugmentPolicy.IMAGENET),
+            transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.IMAGENET),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
         ]
