fix lat lng stats calculation

src/calculate_norm_std.py
@@ -15,17 +15,6 @@ from utils_dataset import DatasetSplitType
 def calculate_norm_std(dataset_dirs, df_path):
     channels_sum, channels_squared_sum, num_batches = 0, 0, 0
 
-    df = pd.read_csv(Path(df_path))
-    df = df[df["uuid"].isna() == False]  # remove rows for which the image doesn't exist
-    map_poly_index_to_y = df.filter(["polygon_index"]).drop_duplicates().sort_values("polygon_index")
-    map_poly_index_to_y["y"] = np.arange(len(map_poly_index_to_y))  # cols: polygon_index, y
-    df = df.merge(map_poly_index_to_y, on="polygon_index")
-
-    lat_mean = df['latitude'].mean()
-    lng_mean = df['longitude'].mean()
-    lat_std = df['latitude'].std()
-    lng_std = df['longitude'].std()
-
     for dataset_dir in dataset_dirs:
         path_images = Path(dataset_dir, "images", DatasetSplitType.TRAIN.value)
         uuid_dir_paths = flatten([glob(str(Path(dataset_dir, "images", DatasetSplitType.TRAIN.value, "*"))) for dataset_dir in dataset_dirs])
@@ -46,4 +35,4 @@ def calculate_norm_std(dataset_dirs, df_path):
     std = (channels_squared_sum / num_batches - mean**2) ** 0.5
     print("Train dataset mean: " + str(mean))
     print("Train dataset std: " + str(std))
-    return mean, std, [lat_mean, lng_mean, lat_std, lng_std]
+    return mean, std