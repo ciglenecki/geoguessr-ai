fix normalization

src/model.py
@@ -295,12 +295,10 @@ class LitModelReg(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         image_list, _, image_true_coords = batch
         y_pred = self(image_list)
-        y_pred_changed = y_pred.apply_(lambda x: math.radians(x))
-        image_true_coords_transformed = [
-            [math.asin(elem[0]*self.data_module.lat_max_sin + self.data_module.lat_min_sin),
-             math.asin(elem[1]*self.data_module.lng_max_sin + self.data_module.lng_min_sin)] for elem in
-            image_true_coords.tolist()]
-        haver_dist = np.mean(haversine_distances(y_pred_changed.cpu(), image_true_coords_transformed))
+
+        y_pred_changed, image_true_coords_transformed = self.normalize_output(y_pred, image_true_coords)
+
+        haver_dist = np.mean(haversine_distances(y_pred_changed.cpu(), image_true_coords_transformed.cpu()))
 
         loss = F.mse_loss(y_pred, image_true_coords)
         data_dict = {
@@ -324,13 +322,10 @@ class LitModelReg(pl.LightningModule):
     def test_step(self, batch, batch_idx):
         image_list, _, image_true_coords = batch
         y_pred = self(image_list)
-        y_pred_changed = y_pred.apply_(lambda x: math.radians(x))
-        image_true_coords_transformed = [
-            [math.asin(elem[0] * self.data_module.lat_max_sin + self.data_module.lat_min_sin),
-             math.asin(elem[1] * self.data_module.lng_max_sin + self.data_module.lng_min_sin)] for elem in
-            image_true_coords.tolist()]
 
-        haver_dist = np.mean(haversine_distances(y_pred_changed.cpu(), image_true_coords_transformed))
+        y_pred_changed, image_true_coords_transformed = self.normalize_output(y_pred, image_true_coords)
+
+        haver_dist = np.mean(haversine_distances(y_pred_changed.cpu(), image_true_coords_transformed.cpu()))
 
         loss = F.mse_loss(y_pred, image_true_coords)
         data_dict = {
@@ -376,6 +371,16 @@ class LitModelReg(pl.LightningModule):
                 "name": None,
             },
         }
+
+    def normalize_output(self, y, coords):
+
+        y[:, 0] = y[:, 0] * self.data_module.lat_max_sin + self.data_module.lat_min_sin
+        y[:, 1] = y[:, 1] * self.data_module.lng_max_sin + self.data_module.lng_min_sin
+
+        coords[:, 0] = coords[:, 0] * self.data_module.lat_max_sin + self.data_module.lat_min_sin
+        coords[:, 1] = coords[:, 1] * self.data_module.lng_max_sin + self.data_module.lng_min_sin
+
+        return y, coords
 
 
 class LitSingleModel(LitModel):