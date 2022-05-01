e3bb7829e3ac49e6b000940db0e6d438ea977760
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2022-05-01 02:07:03 +0200

    add script creating poly

--- src/run_coords_decorate_csv.sh
+++ src/run_coords_decorate_csv.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-basename = python3 src/preprocess_csv_add_classes.py
+basename = python3 src/preprocess_csv_create_polygons.py
 $basename --spacing 0.8;
 $basename --spacing 0.7;
+$basename --spacing 0.65;
 $basename --spacing 0.6;
+$basename --spacing 0.55;
 $basename --spacing 0.5;
+$basename --spacing 0.45;
 $basename --spacing 0.4;
 $basename --spacing 0.3;
-$basename --spacing 0.2;
-$basename --spacing 0.17;
-$basename --spacing 0.15;
-$basename --spacing 0.14;
-$basename --spacing 0.12;
-$basename --spacing 0.1;
-$basename --spacing 0.09;
