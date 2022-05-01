a84253ea66f2a9c7d527741b13a92e53a889360f
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2022-05-01 02:08:35 +0200

    rename variable in script

--- src/run_coords_decorate_csv.sh
+++ src/run_coords_decorate_csv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-basename = python3 src/preprocess_csv_create_polygons.py
-$basename --spacing 0.8;
-$basename --spacing 0.7;
-$basename --spacing 0.65;
-$basename --spacing 0.6;
-$basename --spacing 0.55;
-$basename --spacing 0.5;
-$basename --spacing 0.45;
-$basename --spacing 0.4;
-$basename --spacing 0.3;
+script_name = python3 src/preprocess_csv_create_polygons.py
+$script_name --spacing 0.8;
+$script_name --spacing 0.7;
+$script_name --spacing 0.65;
+$script_name --spacing 0.6;
+$script_name --spacing 0.55;
+$script_name --spacing 0.5;
+$script_name --spacing 0.45;
+$script_name --spacing 0.4;
+$script_name --spacing 0.3;
