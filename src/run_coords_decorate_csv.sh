08f84d170e6152e6fff1a7b31c9c0f390cbef36c
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2022-05-01 02:09:59 +0200

    add ""

--- src/run_coords_decorate_csv.sh
+++ src/run_coords_decorate_csv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-script_name=python3 src/preprocess_csv_create_polygons.py
+script_name="python3 src/preprocess_csv_create_polygons.py"
 $script_name --spacing 0.8;
 $script_name --spacing 0.7;
 $script_name --spacing 0.65;
