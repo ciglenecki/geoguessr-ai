b4a208c5a788ae2140bb053147ab00daf014d1c2
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2022-05-01 14:13:35 +0200

    push lr change

--- src/model.py
+++ src/model.py
@@ -188,17 +188,17 @@ class LitModelClassification(pl.LightningModule):
         )
         return {
             "optimizer": optimizer,
-            "lr_scheduler": {
-                # "scheduler": scheduler,
-                # The unit of the scheduler's step size, could also be 'step', 'epoch' updates the scheduler on epoch end whereas 'step', updates it after a optimizer update
-                "interval": "epoch",
-                "monitor": "val/loss",
-                # How many epochs/steps should pass between calls to `scheduler.step()`.1 corresponds to updating the learning  rate after every epoch/step.
-                # If "monitor" references validation metrics, then "frequency" should be set to a multiple of "trainer.check_val_every_n_epoch".
-                "frequency": 1,
-                # If using the `LearningRateMonitor` callback to monitor the learning rate progress, this keyword can be used to specify a custom logged name
-                "name": None,
-            },
+            # "lr_scheduler": {
+            #     "scheduler": scheduler,
+            #     # The unit of the scheduler's step size, could also be 'step', 'epoch' updates the scheduler on epoch end whereas 'step', updates it after a optimizer update
+            #     "interval": "epoch",
+            #     "monitor": "val/loss",
+            #     # How many epochs/steps should pass between calls to `scheduler.step()`.1 corresponds to updating the learning  rate after every epoch/step.
+            #     # If "monitor" references validation metrics, then "frequency" should be set to a multiple of "trainer.check_val_every_n_epoch".
+            #     "frequency": 1,
+            #     # If using the `LearningRateMonitor` callback to monitor the learning rate progress, this keyword can be used to specify a custom logged name
+            #     "name": None,
+            # },
         }
 
 
