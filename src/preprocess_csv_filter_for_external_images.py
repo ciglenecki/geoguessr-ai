534f80ed6c8195f43ba5e856d1b7a15cb6da561a
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2022-05-06 22:51:19 +0200

    fix preprocess

--- src/preprocess_csv_filter_for_external_images.py
+++ src/preprocess_csv_filter_for_external_images.py
@@ -27,7 +27,7 @@ from pathlib import Path
 
 import pandas as pd
 
-from utils_functions import is_valid_dir
+from utils_functions import flatten, is_valid_dir
 from utils_paths import PATH_DATA_EXTERNAL, PATH_DATA_SAMPLER
 
 
@@ -42,11 +42,12 @@ def parse_args(args):
     )
 
     parser.add_argument(
-        "--data-dir",
+        "--images-dirs",
         metavar="dir",
-        type=is_valid_dir,
-        help="Directory with images from the external source",
-        default=Path(PATH_DATA_EXTERNAL, "data"),
+        type=str,
+        help="Image directories (directories with UUIDs) for which the norm and std will be caculated.",
+        nargs="+",
+        required=True,
     )
     parser.add_argument(
         "--out",
@@ -60,7 +61,7 @@ def parse_args(args):
 
 def main(args):
     args = parse_args(args)
-    uuids = os.listdir(args.data_dir)
+    uuids = flatten([os.listdir(image_dir) for image_dir in args.image_dirs])
     df = pd.read_csv(args.base_csv)
     df = df.loc[df["uuid"].isin(uuids), :]
     print("Saving dataframe with {} rows to {}".format(len(df), args.out))
